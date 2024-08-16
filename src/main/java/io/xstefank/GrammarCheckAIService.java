49dc23079d20e59ecc9251971005bb1608e91d67
xstefank <xstefank122@gmail.com>
2024-08-16 09:16:57 +0200

    Fix the text passing to AI

--- src/main/java/io/xstefank/GrammarCheckAIService.java
+++ src/main/java/io/xstefank/GrammarCheckAIService.java
@@ -12,7 +12,7 @@ public interface GrammarCheckAIService {
         Fix the grammar in the following text, which is in the {language} language.
         Output only the fixed text as a string without quotes.
         Newline characters should be outputted Linux compliant which is as '\n'.
-        The text in which you should fix grammar errors is {text}.
+        The text in which you should fix grammar errors is "{text}".
         """)
     String fixGrammar(String language, String text);
 }
