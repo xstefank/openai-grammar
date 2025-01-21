ff5df3c92ed92e72f2a75c91a397daf22d5f9d3f
xstefank <xstefank122@gmail.com>
2025-01-21 08:32:28 +0100

    Add preservation of newlines to the prompt

--- src/main/java/io/xstefank/GrammarCheckAIService.java
+++ src/main/java/io/xstefank/GrammarCheckAIService.java
@@ -12,6 +12,7 @@ public interface GrammarCheckAIService {
         Fix the grammar in the following text, which is in the {language} language.
         Output only the fixed text as a string without quotes.
         Newline characters should be outputted Linux compliant which is as '\n'.
+        Preserve all lines even if they are empty.
         The text in which you should fix grammar errors is "{text}".
         """)
     String fixGrammar(String language, String text);
