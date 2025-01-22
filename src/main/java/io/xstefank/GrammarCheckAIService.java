e6ebb6170e4aa775891d991c8f5c3086f0f2af95
xstefank <xstefank122@gmail.com>
2025-01-22 15:55:33 +0100

    Another try to preseve empty lines

--- src/main/java/io/xstefank/GrammarCheckAIService.java
+++ src/main/java/io/xstefank/GrammarCheckAIService.java
@@ -12,7 +12,7 @@ public interface GrammarCheckAIService {
         Fix the grammar in the following text, which is in the {language} language.
         Output only the fixed text as a string without quotes.
         Newline characters should be outputted Linux compliant which is as '\n'.
-        Preserve all lines even if they are empty.
+        Empty lines must be kept empty and not deleted.
         Don't return text with empty new line at the end so the copy-paste of the text ends in the same place.
         The text in which you should fix grammar errors is "{text}".
         """)
